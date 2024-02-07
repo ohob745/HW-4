2a7cc2cf2e38207973a323e8171ed4cfd2af6c3f
Alawashez2 <mohmmd20111@hotmail.com>
2024-02-07 14:43:44 +0300

    create

---
+++ lib/data/product_list.dart
@@ -0,0 +1,8 @@
+import 'package:hw4/models/product.dart';
+
+List<Product> productList = [
+  Product("Apple", 3800, " ", "iphone 14"),
+  product1,
+];
+
+Product product1 = Product("Apple", 3800, " ", "iphone 14");
---
+++ lib/models/product.dart
@@ -0,0 +1,7 @@
+class Product {
+  String name;
+  double price;
+  String image;
+  String brand;
+  Product(this.brand, this.price, this.image, this.name);
+}
